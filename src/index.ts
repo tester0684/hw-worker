97b5fa4824e23aa282309424de01c4c36ecc93fd
tester0684 <nimbusdevelop.work@gmail.com>
2025-11-12 16:33:32 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 8");
+    const stmt = env.DB.prepare("SELECT * FROM HotWheels LIMIT 20");
     const { results } = await stmt.all();
 
     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
